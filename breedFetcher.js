2e6cb6ea5005d95c53f45024179f58a4b167ea35
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-09 12:23:50 -0700

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,23 @@
-const request = require("request");
-const fs = require('fs');
+const request = require('request');
 
-const catBreed = process.argv.slice(2);
-const catName = (catBreed[0].toLowerCase());
-
-const requestWrapper = function() {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, (error, response, body) => {
-
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    
     if (error) {
-      console.log("error. check your URL and make sure it is correct");
+      callback(error);
       return;
     }
 
+    
     const data = JSON.parse(body);
-
+    
     if (data[0] === undefined) {
-      console.log("This cats name is not in our index. try again");
+      callback("Cat name is not describe");
     } else {
-      console.log(data[0].description);
+      callback(null, data[0].description);
     }
-
   });
 };
 
-if (catName) {
-  requestWrapper();
-} else {
-  console.log("please give me the name of a cat");
-}
+
+module.exports = { fetchBreedDescription };
